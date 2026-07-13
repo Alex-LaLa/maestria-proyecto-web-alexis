a9570bb50c185915492358edad1e897518941bda
Alexis <alexander17179@gmail.com>
2026-07-13 13:04:19 -0600

    Avances-Sesion-2-Guia-Practica

---
+++ frontend-app/src/app/pages/login/login.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  constructor(private router: Router) {}
+  ingresarSistema() {
+    this.router.navigate(['/dashboard']);
+  }
+}
