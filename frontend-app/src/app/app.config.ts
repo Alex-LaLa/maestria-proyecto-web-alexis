89f9554ff7bc84424dd3c6a40211f1691108c51c
Alexis <alexander17179@gmail.com>
2026-07-27 22:56:59 -0600

    Se agrego lo de la guia, pero como estaba avanzado tambien cuenta para la tarea

--- frontend-app/src/app/app.config.ts
+++ frontend-app/src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(), provideHttpClient()
+    provideRouter(routes), provideClientHydration(), provideHttpClient(), provideMarkdown()
   ]
 };
